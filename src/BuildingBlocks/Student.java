3b8afb736c342efc6ee5933e5804e2fb10c23cce
Sanket <sanket.gupte542@gmail.com>
2025-06-29 21:20:00 +0530

    Initial Commit

---
+++ src/BuildingBlocks/Student.java
@@ -0,0 +1,12 @@
+package BuildingBlocks;
+//Simplest Java Class
+public class Student {
+
+    //in file Student.java
+    String name; //variable
+
+    //method or function
+    public String getName() { return name; }
+
+    public void setName(String name){ this.name = name; }
+}
