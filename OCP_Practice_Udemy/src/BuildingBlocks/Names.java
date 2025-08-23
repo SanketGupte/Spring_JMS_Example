c94e724d9fa299ac4530b38560bdedcd559d926f
Sanket <sanket.gupte542@gmail.com>
2025-08-23 23:22:30 +0530

    initial commit

---
+++ OCP_Practice_Udemy/src/BuildingBlocks/Names.java
@@ -0,0 +1,8 @@
+package BuildingBlocks;
+
+public class Names {
+    public static void main(String[] args) {
+        System.out.println("FirstName: " + args[0]);
+        System.out.println("LastName: " + args[1]);
+    }
+}
