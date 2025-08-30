65a6a93e8409bc9a5398606cb8242834216d3602
Sanket Gupte <sanket.gupte542@gmail.com>
2025-08-30 14:58:48 +0530

    Removal of unrelated files

--- OCP_Practice_Udemy/src/BuildingBlocks/Names.java
+++
@@ -1,8 +0,0 @@
-package BuildingBlocks;
-
-public class Names {
-    public static void main(String[] args) {
-        System.out.println("FirstName: " + args[0]);
-        System.out.println("LastName: " + args[1]);
-    }
-}
